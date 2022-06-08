d653ed96522e9db1e44ee0d2a04d5a23529cf6fb
cheryl <cheryls@brdata.com>
2022-06-08 16:44:04 -0400

    Add project files.

---
+++ Patients/Models/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Patients.Models
+{
+    public class Patient
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Birthday { get; set; }
+        public char Gender { get; set; }
+    }
+}
